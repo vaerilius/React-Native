3877e8e8b2ff4ad5ebc1f2c8fb584cacf5e7d028
vae <tjtamminen@pm.me>
2021-05-25 11:41:33 +0300

    10.3

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import RepositoryList from './src/components/Repository/RepositoryList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <RepositoryList />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 10,
   },
 });
